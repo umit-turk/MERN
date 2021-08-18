5866a695e10664100d743e17942aea4e037f1d8d
Umit Turk <uyt.turk@gmail.com>
2021-08-19 01:39:28 +0300

    mongoDB- Login-register

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+
+dotenv.config();
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("connected to MongoDb"))
+  .catch((err) => console.log(err));
+
+
+  app.use("/api/auth", authRoute);
+
+dotenv.config();
+
+app.listen("5000", () => {
+  console.log("Backend is running");
+});
