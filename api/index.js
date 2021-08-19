1828e7e049226a40b90a44b9c5c394c779ac8599
Umit Turk <uyt.turk@gmail.com>
2021-08-19 15:47:09 +0300

    user crud

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 
 dotenv.config();
@@ -19,6 +20,7 @@ mongoose
 
 
   app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
 
 dotenv.config();
 
