50c0f34f4f5bfac40b9a50ebaed69ba62f20fd40
Umit Turk <uyt.turk@gmail.com>
2021-08-19 18:18:18 +0300

    upload file

--- api/index.js
+++ api/index.js
@@ -5,10 +5,11 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
-app.use(express.json())
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -19,10 +20,24 @@ mongoose
   .then(console.log("connected to MongoDb"))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-  app.use("/api/auth", authRoute);
-  app.use("/api/users", userRoute);
-  app.use("/api/posts", postRoute);
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 dotenv.config();
 
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -16,48 +16,48 @@ router.post("/", async (req, res) => {
 //UPDATE POST
 
 router.put("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if (post.username === req.body.username) {
-        try {
-          const updatedPost = await Post.findByIdAndUpdate(
-            req.params.id,
-            {
-              $set: req.body,
-            },
-            { new: true }
-          );
-          res.status(200).json(updatedPost);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      } else {
-        res.status(401).json("You can update only your post!");
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
       }
-    } catch (err) {
-      res.status(500).json(err);
+    } else {
+      res.status(401).json("You can update only your post!");
     }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //DELETE POST
 
 router.delete("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if (post.username === req.body.username) {
-        try {
-          await post.delete();
-          res.status(200).json("Post has been deleted...")
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      } else {
-        res.status(401).json("You can delete only your post!");
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
       }
-    } catch (err) {
-      res.status(500).json(err);
+    } else {
+      res.status(401).json("You can delete only your post!");
     }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //GET POST
 router.get("/:id", async (req, res) => {
@@ -71,13 +71,25 @@ router.get("/:id", async (req, res) => {
 
 //GET ALL POSTS
 router.get("/", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      res.status(200).json(post);
-    } catch (err) {
-      res.status(500).json(err);
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
     }
-  });
-
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
